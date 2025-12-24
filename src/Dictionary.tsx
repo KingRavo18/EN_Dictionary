24754c71a44af8cf79b111d72b0724e012eb7311
KingRavo18 <rkingovskis14@gmail.com>
2025-12-24 17:00:24 +0200

    k

--- src/Dictionary.tsx
+++ src/Dictionary.tsx
@@ -1,9 +1,22 @@
-import type { JSX } from "react";
+import { useState, type JSX } from "react";
 
 export default function Dictionary(): JSX.Element{
+    const [word, setWord] = useState("");
+
+    function fetchDescribeResults(): void{
+
+    }
+
     return(
-        <div>
-            <p></p>
-        </div>
+        <main className="w-full flex justify-center pt-[5%]">
+            <input 
+                type="text" 
+                value={word} 
+                onChange={event => setWord(event.target.value)} 
+                placeholder="Enter word"
+                className="py-0.5 px-2.5 text-[130%] border-b-2 border-b-[#bbbbbb]"
+            />
+            <button onClick={fetchDescribeResults}>Find</button>
+        </main>
     );
 }
