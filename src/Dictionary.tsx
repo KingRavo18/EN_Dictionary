8725d2616089d46fece8f5cbfd04b0eaf79bfee6
KingRavo18 <rkingovskis14@gmail.com>
2025-12-26 00:23:49 +0200

    g

--- src/Dictionary.tsx
+++ src/Dictionary.tsx
@@ -1,42 +1,51 @@
 import { useState, type JSX } from "react";
 
 type WordData = {
-    word: string
+    word?: string,
+    definition?: string,
 }
 
 export default function Dictionary(): JSX.Element{
     const [searchedWord, setSearchedWord] = useState<string>("");
     const [wordData, setWordData] = useState<WordData | undefined>(undefined);
+    const [errorMessage, setErrorMessage] = useState<string | undefined>(undefined);
 
     async function fetchDescribeResults(): Promise<void>{
-        if(searchedWord.trim() === ""){
+        if(searchedWord.trim() === "" || wordData?.word === searchedWord){
             return;
         }
+        console.log();
         try{
             const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchedWord}`);
             if(!response.ok){
                 throw new Error("This word does not exist");
             }
             const data = await response.json();
-            const [{ word }] = data;
+            const [{ 
+                word,
+                meanings: [{ definitions: [{ definition }]}] 
+            }] = data;
             setWordData({
                 word: word,
-
+                definition: definition,
             });
         }
         catch(error){
-            console.error(error);
+            setWordData(undefined);
+            setErrorMessage((error as Error).message);
         }
     }
 
     return(
-        <main className="w-full flex flex-col items-center pt-[5%]">
-            <div className="flex items-center">
+        <main className="w-full flex flex-col items-center pt-[2.5%]">
+            <h1 className="font-bold text-[200%]">DICTONARY</h1>
+            <div className="flex items-center mt-[2.5%]">
                 <input 
                     type="text" 
                     value={searchedWord} 
                     onChange={event => setSearchedWord(event.target.value)} 
                     placeholder="Search..."
+                    autoComplete="off"
                     className="py-0.5 px-2.5 text-[130%] border-b-2 cursor-pointer transition-[2s]
                             border-b-[#bbbbbb] hover:border-b-[#7c7c7c] hover:bg-[#f1f1f1]"
                 />
@@ -50,7 +59,16 @@ export default function Dictionary(): JSX.Element{
                 </button>
             </div>
             {wordData && 
-                <div>{wordData.word}</div>
+                (
+                    <div className="mt-[2.5%]">
+                        <h2 className="font-bold text-center">{wordData.word}</h2>
+                        <p>{wordData.definition}</p>
+                    </div>
+                )
+                ||
+                (
+                    <p>{errorMessage}</p>
+                )
             }
         </main>
     );
