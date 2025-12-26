68926b9c5bef17f178efc277c62b49a660b14e96
KingRavo18 <rkingovskis14@gmail.com>
2025-12-26 19:22:29 +0200

    f

--- src/Dictionary.tsx
+++ src/Dictionary.tsx
@@ -1,38 +1,40 @@
 import { useState, type JSX } from "react";
 
+type Definition = {
+    definition: string;
+}
+type Meaning = {
+    partOfSpeech: string;
+    definitions: Definition[];
+}
 type WordData = {
-    word?: string,
-    definition?: string,
-
+    word?: string;
+    meanings?: Meaning[];
 }
 
 export default function Dictionary(): JSX.Element{
     const [searchedWord, setSearchedWord] = useState<string>("");
-    const [wordData, setWordData] = useState<WordData | undefined>(undefined);
-    const [errorMessage, setErrorMessage] = useState<string | undefined>(undefined);
+    const [wordData, setWordData] = useState<WordData | null>(null);
+    const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
     async function fetchDescribeResults(): Promise<void>{
         if(searchedWord.trim() === "" || wordData?.word === searchedWord){
             return;
         }
-        console.log();
         try{
             const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchedWord}`);
             if(!response.ok){
                 throw new Error("This word does not exist");
             }
-            const data = await response.json();
-            const [{ 
-                word,
-                meanings: [{ definitions: [{ definition }]}] 
-            }] = data;
+            const [{ word, meanings }] = await response.json();
+            setErrorMessage(null);
             setWordData({
                 word: word.charAt(0).toUpperCase() + word.slice(1),
-                definition: definition,
+                meanings: meanings,
             });
         }
         catch(error){
-            setWordData(undefined);
+            setWordData(null);
             setErrorMessage((error as Error).message);
         }
     }
@@ -40,46 +42,44 @@ export default function Dictionary(): JSX.Element{
     return(
         <main className="w-full flex flex-col items-center pt-[2.5%]">
             <h1 className="font-bold text-[200%]">DICTONARY</h1>
-            <div className="flex items-center mt-[2.5%]">
-                <input 
-                    type="text" 
-                    value={searchedWord} 
-                    onChange={event => setSearchedWord(event.target.value)} 
-                    placeholder="Search..."
-                    autoComplete="off"
-                    className="py-0.5 px-2.5 text-[130%] border-b-2 cursor-pointer transition-[2s]
-                            border-b-[#bbbbbb] hover:border-b-[#7c7c7c] hover:bg-[#f1f1f1]"
+            <div className="flex items-center mt-[2.5%]"> 
+                <input type="text" 
+                       value={searchedWord} 
+                       onChange={event => setSearchedWord(event.target.value)} 
+                       placeholder="Search..."
+                       autoComplete="off"
+                       className="py-0.5 px-2.5 text-[130%] border-b-2 cursor-pointer transition-[2s]
+                       border-b-[#bbbbbb] hover:border-b-[#7c7c7c] hover:bg-[#f1f1f1]"
                 />
-                <button 
-                    onClick={fetchDescribeResults}
-                    className="py-0.5 px-2.5 text-[130%] 
-                            cursor-pointer material-symbols-outlined transition-[2s]
-                            text-[#949494] hover:text-[#666666]"
+                <button onClick={fetchDescribeResults}
+                        className="py-0.5 px-2.5 text-[130%] 
+                        cursor-pointer material-symbols-outlined transition-[2s]
+                        text-[#949494] hover:text-[#666666]"
                 >
                     search
                 </button>
             </div>
-            {wordData && 
-                (
-                    <div className="mt-[2.5%] text-center">
-                        <h2 className="font-bold text-[150%] 
-                                     bg-[#f2ffb7] rounded-[5px] border-2 border-[#bcbd99]"
-                        >
-                            {wordData.word}
-                        </h2>
-                        <p className="mt-[2.5%] w-75 bg-[#f2ffb7] rounded-[5px] border-2 border-[#bcbd99]">
-                            {wordData.definition}
-                        </p>
+            {!errorMessage && wordData && 
+                <div className="mt-[2.5%] text-center">
+                    <h2 className="font-bold text-[150%] dictionary_window">
+                        {wordData.word}
+                    </h2>
+                    {wordData.meanings?.map((meaning_item, index) => 
+                        <div key={index}>
+                            <p className="mt-[5%] w-75 dictionary_window">
+                                {meaning_item.partOfSpeech}
+                            </p>
+                            <p className="w-75 dictionary_window">
+                                {meaning_item.definitions[0].definition}
+                            </p>
+                        </div>
+                    )}
                     </div>
-                )
-                ||
-                (
-                    <p className="w-75 mt-[2.5%] text-center 
-                                bg-[#fce0db] rounded-[5px] border-2 border-[#fc8772]"
-                    >
-                        {errorMessage}
-                    </p>
-                )
+            }
+            {!wordData && errorMessage && 
+                <p className="w-75 mt-[2.5%] text-center bg-[#fce0db] rounded-[5px] border-2 border-[#fc8772]">
+                    {errorMessage}
+                </p>
             }
         </main>
     );
